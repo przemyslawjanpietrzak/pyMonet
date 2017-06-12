f6c04edaa2527c837a8d7e092a6825992e5a02e7
przemyslaw <przemyslaw.jan.pietrzak@gmail.com>
2017-06-12 20:40:42 +0200

    implement some semigroups

---
+++ monetPy/semigroups.py
@@ -0,0 +1,35 @@
+class Semigroup:
+
+    def __init__(self, value):
+        self.value = value
+
+    def fold(self, fn):
+        return fn(self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+class Sum(Semigroup):
+
+    def concat(self, semigroup):
+        return Sum(self.value + semigroup.value)
+
+
+class All(Semigroup):
+
+    def concat(self, semigroup):
+        return All(self.value and semigroup.value)
+
+
+class First(Semigroup):
+
+    def concat(self, semigroup):
+        return First(self.value)
+
+
+class Map(Semigroup):
+
+    def concat(self, semigroup):
+        return Map(
+            {key: value.concat(semigroup.value[key]) for key, value in self.value.items()}
+        )
---
+++ monetPy/test_semigroups.py
@@ -0,0 +1,53 @@
+import pytest
+from monetPy.semigroups import Sum, All, First, Map
+from monetPy.utils import identity
+
+
+ingredient1 = Map({'score': Sum(1), 'won': All(True), 'captain': First('captain america')})
+ingredient2 = Map({'score': Sum(2), 'won': All(True), 'captain': First('iron man')})
+ingredient3 = Map({'score': Sum(3), 'won': All(False), 'captain': First('Batman')})
+
+
+def test_sum():
+    assert Sum(1).concat(Sum(2)) == Sum(3)
+    assert Sum(1).concat(
+        Sum(2).concat(Sum(3))
+    ) == Sum(6)
+    assert Sum(1).concat(Sum(2)).concat(Sum(3)) == Sum(6)
+
+
+def test_all():
+    assert All(True).concat(All(True)) == All(True)
+    assert All(True).concat(All(False)) == All(False)
+
+    assert All(True).concat(
+        All(True).concat(All(True))
+    ) == All(True)
+    assert All(True).concat(All(True)).concat(All(True)) == All(True)
+
+    assert All(True).concat(
+        All(False).concat(All(True))
+    ) == All(False)
+    assert All(True).concat(All(True)).concat(All(False)) == All(False)
+
+
+def test_first():
+    assert First('first').concat(First('second')) == First('first')
+    assert First('first').concat(First('second')).concat(First('third')) == First('first')
+    assert First('first').concat(
+        First('second').concat(First('third'))
+    ) == First('first')
+
+
+def test_map3():
+    assert ingredient1.concat(ingredient2) == Map({'score': Sum(3), 'won': All(True), 'captain': First('captain america')})
+
+
+def test_map1():
+    assert ingredient1.concat(ingredient2).concat(ingredient3) == Map(
+        {'score': Sum(6), 'won': All(False), 'captain': First('captain america')})
+
+
+def test_map2():
+    assert ingredient1.concat(ingredient2.concat(ingredient3))== Map(
+        {'score': Sum(6), 'won': All(False), 'captain': First('captain america')})
