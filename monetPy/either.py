be929eefefa62dfc288f451d0694da1c90f36a3d
przemyslaw <przemyslaw.jan.pietrzak@gmail.com>
2017-06-26 23:54:50 +0200

    add docs for isRight

--- monetPy/either.py
+++ monetPy/either.py
@@ -1,5 +1,8 @@
 class Either:
-
+    """
+    The Either type represents values with two possibilities: B value of type Either<A, B> is either Left<A> or Right<B>
+    But not both in the same time.
+    """
     def __init__(self, value):
         self.value = value
 
@@ -7,6 +10,14 @@ class Either:
 class Left(Either):
 
     def map(self, left_fn, right_fn):
+        """
+        takes 2 functions and applied first one on current Either value and returns new left Either mapped value
+        :param left_fn: mapper for left value of Either
+        :type (A) -> B
+        :param right_fn: mapper for right value of Either, never called
+        :type: (Any) -> Any
+        :return: Left<B>
+        """
         return Left(left_fn(self.value))
 
     def fold(self, left_fn, right_fn):
@@ -16,6 +27,14 @@ class Left(Either):
 class Right(Either):
 
     def map(self, left_fn, right_fn):
+        """
+        takes 2 functions and applied second one on current Either value and returns new left Either mapped value
+        :param left_fn: mapper for left value of Either, never called
+        :type: (Any) -> Any
+        :param right_fn: mapper for right value of Either
+        :type (A) -> B
+        :return: Left<B>
+        """
         return Right(right_fn(self.value))
 
     def fold(self, left_fn, right_fn):
