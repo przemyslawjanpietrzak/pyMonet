bb9147b35dd041c6970469105d093a2e951eb7da
przemyslaw <przemyslaw.pietrzak@f-secure.com>
2017-07-10 20:25:45 +0200

    refactor either

--- pymonet/test_either.py
+++ pymonet/test_either.py
@@ -1,7 +1,16 @@
 import pytest
 
 from pymonet.either import Either, Left, Right
-from pymonet.utils import identity
+from pymonet.utils import increase
+
+
+class EitherSpy;
+
+    def error_handler(value):
+        pass
+
+    def success_handler(value):
+        pass
 
 
 def test_either_eq_operator_should_compare_values():
@@ -17,8 +26,8 @@ def test_either_eq_operator_should_compare_values():
 
 def test_mapper_should_be_applied_only_on_current_value():
 
-    assert Left(42).map(lambda value: value + 1, identity) == Left(43)
-    assert Right(42).map(identity, lambda value: value + 1) == Right(43)
+    assert Left(42).map(increase) == Left(42)
+    assert Right(42).map(increase) == Right(43)
 
 
 def test_is_right_should_return_suitable_value():
@@ -34,5 +43,27 @@ def test_is_left_should_return_suitable_value():
 
 def test_fold_should_be_applied_only_on_current_value_and_return_value():
 
-    assert Left(42).fold(lambda value: value + 1, identity) == 43
-    assert Right(42).fold(identity, lambda value: value + 1) == 43
+    assert Left(42).fold(increase) == 42
+    assert Right(42).fold(increase) == 43
+
+def test_case_method_should_call_proper_handler(mocker):
+    either_spy = EitherSpy()
+    mocker.spy(either_spy, 'error_handler')
+    mocker.spy(either_spy, 'success_handler')
+
+    Left(42).case(
+        success=either_spy.success_handler,
+        error=either_spy.error_handler
+    )
+
+    assert either_spy.error_handler.call_count == 1
+    assert either_spy.success_handler.call_count == 0
+
+
+    Roght(42).case(
+        success=either_spy.success_handler,
+        error=either_spy.error_handler
+    )
+
+    assert either_spy.error_handler.call_count == 1
+    assert either_spy.success_handler.call_count == 1
