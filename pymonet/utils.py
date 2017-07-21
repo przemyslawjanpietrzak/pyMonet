fda88858f7e2cf66101bfc3e77e432c5afc99183
przemyslaw <przemyslaw.pietrzak@f-secure.com>
2017-07-21 18:27:49 +0200

    add compose and pipe

--- pymonet/utils.py
+++ pymonet/utils.py
@@ -1,2 +1,31 @@
-identity = lambda value: value
-increase = lambda value: value + 1
+from functools import reduce
+
+
+def identity(value):
+    return value
+
+
+def increase(value):
+    return value + 1
+
+
+def compose(value, *functions):
+    return reduce(
+        lambda current_value, function: function(current_value),
+        functions[::-1],
+        value
+    )
+
+def pipe(value, *functions):
+    return reduce(
+        lambda current_value, function: function(current_value),
+        functions,
+        value
+    )
+
+def curried_map(mapper):
+    return lambda lst: list(map(mapper, lst))
+
+
+def curried_filter(filterer):
+    return lambda lst: list(filter(filterer, lst))
