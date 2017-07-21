2a6a2acdf839b58f17044eeb2d8ef3533d1e8d42
przemyslaw <przemyslaw.pietrzak@f-secure.com>
2017-07-21 18:37:20 +0200

    add doc

--- pymonet/utils.py
+++ pymonet/utils.py
@@ -9,7 +9,23 @@ def increase(value):
     return value + 1
 
 
+def curried_map(mapper):
+    return lambda lst: list(map(mapper, lst))
+
+
+def curried_filter(filterer):
+    return lambda lst: list(filter(filterer, lst))
+
+
 def compose(value, *functions):
+    """
+    Performs right-to-left function composition.
+    :param value - argument of first applied function
+    :type Any
+    :param functions - list of functions to applied from right-to-left
+    :return result of all functions
+    :typa Any
+    """
     return reduce(
         lambda current_value, function: function(current_value),
         functions[::-1],
@@ -17,15 +33,16 @@ def compose(value, *functions):
     )
 
 def pipe(value, *functions):
+    """
+    Performs ledt-to-right function composition.
+    :param value - argument of first applied function
+    :type Any
+    :param functions - list of functions to applied from ledt-to-right
+    :return result of all functions
+    :typa Any
+    """
     return reduce(
         lambda current_value, function: function(current_value),
         functions,
         value
     )
-
-def curried_map(mapper):
-    return lambda lst: list(map(mapper, lst))
-
-
-def curried_filter(filterer):
-    return lambda lst: list(filter(filterer, lst))
