39f3cf811e35c45907286c8b494df3e0ba02acb1
przemyslaw <przemyslaw.jan.pietrzak@gmail.com>
2017-11-07 00:49:00 +0100

    add monad law test to box

--- pymonet/monad_law_tester.py
+++ pymonet/monad_law_tester.py
@@ -1,16 +1,14 @@
 def _get_monadic_value(monad, monad_value, use_constructor=True):
-    return monad.__init__(monad_value) if use_constructor else monad(monad_value)
+    return monad(monad_value)
 
 
 def get_associativity_test(monadic_value, mapper1, mapper2):
     def result():
-        value1 = monadic_value\
-            .bind(mapper1)\
-            .bind(mapper2)
-
-        value2 = monadic_value \
-            .bind(mapper2) \
-            .bind(mapper1)
+        value1 = (monadic_value
+                  .bind(mapper1)).bind(mapper2)
+        value2 = (monadic_value
+                  .bind(mapper2)
+                  .bind(mapper1))
 
         assert value1 == value2
 
@@ -19,13 +17,13 @@ def get_associativity_test(monadic_value, mapper1, mapper2):
 
 def get_left_unit_test(monad, monad_value, mapper, value, use_constructor=True):
     def result():
-        monadic_value = _get_monad_value(monad, monad_value, use_constructor)
-        assert monadic_value.bind(mapper)(value) == mapper(value)
+        monadic_value = _get_monadic_value(monad, monad_value, use_constructor)
+        assert monadic_value.bind(mapper) == mapper(value)
 
     return result
 
 
-def get_right_unit_data(monad, monad_value, use_constructor=False):
+def get_right_unit_data(monad, monad_value, use_constructor=True):
     def result():
         monadic_value = _get_monadic_value(monad, monad_value, use_constructor)
         monadic_value.bind(lambda value: _get_monadic_value(monad, value, use_constructor)) == monadic_value
--- pymonet/test_box.py
+++ pymonet/test_box.py
@@ -1,5 +1,5 @@
 from pymonet.box import Box
-from pymonet.monadlaw_tester import get_associativity_test, get_left_unit_test, get_right_unit_data
+from pymonet.monad_law_tester import get_associativity_test, get_left_unit_test, get_right_unit_data
 from pymonet.utils import increase, identity
 
 
@@ -24,17 +24,20 @@ def test_map_should_return_new_instance_of_box():
 
 def test_fold_should_return_result_of_fold_function_called_with_box_value():
     box = Box(42)
-    assert box.fold(lambda value: value + 1) == 43
+    assert box.bind(lambda value: Box(value + 1)) == Box(43)
 
 
 def test_maybe_associativity_law():
-    get_associativity_test(Box(42), increase, identity)
+    get_associativity_test(
+        monadic_value=Box(42),
+        mapper1=lambda value: Box(value + 1),
+        mapper2=lambda value: Box(value + 2)
+    )()
 
 
 def test_maybe_left_unit_law():
-    get_left_unit_test(Box, 42, increase, 42)
+    get_left_unit_test(Box, 42, increase, 42)()
 
 
 def test_maybe_right_unit_data_law():
-    get_right_unit_data(Box, 42)
-
+    get_right_unit_data(Box, 42)()
