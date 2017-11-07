ed0a10b76dc3bc20be3e2d40ddb4cb6998acc1f2
przemyslaw <przemyslaw.jan.pietrzak@gmail.com>
2017-11-07 02:00:13 +0100

    refactor applicative

--- pymonet/applicative.py
+++ pymonet/applicative.py
@@ -10,21 +10,21 @@ class Applicative:
         """
         self.constructor_fn = constructor_fn
 
-    def map(self, fn):
+    def map(self, mapper):
         """
-        takes function (A) -> A and returns new LazyBox with mapped argument of Applicative constructor function.
+        takes function (A) -> A and returns new Applicative with mapped argument of Applicative constructor function.
         Both mapper end constructor will be called only during calling fold method
-        :param fn: mapper function
-        :type   (constructor_fn) -> B
-        :return: Applicative<() -> fn(constructor_fn)>
+        :param mapper: mapper function
+        :type   (constructor_mapper) -> B
+        :return: Applicative<() -> mapper(constructor_fn)>
         """
-        return Applicative(lambda: fn(self.constructor_fn()))
+        return Applicative(lambda *args: mapper(self.constructor_fn(*args)))
 
-    def fold(self, fn):
+    def fold(self, fn, *args):
         """
         takes function and call constructor function passing returned value to fn function.
         It's only way to call function store in Applicative
         :param fn: (constructor_fn) -> B
         :return: B
         """
-        return fn(self.constructor_fn())
+        return fn(self.constructor_fn(*args))
--- pymonet/test_applicative.py
+++ pymonet/test_applicative.py
@@ -3,33 +3,36 @@ from pymonet.applicative import Applicative
 
 class ApplicativeSpy:
 
+    def mapper(self, input):
+        return input + 1
+
     def fn(self):
         return 42
 
-    def side_effect(self, input):
-        return input
 
-
-def test_applicative(mocker):
-    """
-    input function and all map functions should be called during called fold method
-    """
+def test_applicative_should_call_stored_function_during_fold_method_call(mocker):
 
     applicative_spy = ApplicativeSpy()
     mocker.spy(applicative_spy, 'fn')
-    mocker.spy(applicative_spy, 'side_effect')
 
     applicative = Applicative(applicative_spy.fn)
+
     assert applicative_spy.fn.call_count == 0
 
-    applicative = applicative.map(applicative_spy.side_effect)
-    assert applicative_spy.fn.call_count == 0
-    assert applicative_spy.side_effect.call_count == 0
+    assert applicative.fold(lambda number: number + 1) == 43
+    assert applicative_spy.fn.call_count == 1
 
-    applicative = applicative.map(lambda number: number + 1)
-    assert applicative_spy.fn.call_count == 0
-    assert applicative_spy.side_effect.call_count == 0
+
+def test_applicative_should_call_mapper_during_fold_method_call(mocker):
+
+    applicative_spy = ApplicativeSpy()
+    mocker.spy(applicative_spy, 'fn')
+    mocker.spy(applicative_spy, 'mapper')
+
+    applicative = Applicative(applicative_spy.fn).map(applicative_spy.mapper)
+
+    assert applicative_spy.mapper.call_count == 0
 
     assert applicative.fold(lambda number: number + 1) == 44
-    assert applicative_spy.fn.call_count == 1
-    assert applicative_spy.side_effect.call_count == 1
+    assert applicative_spy.mapper.call_count == 1
+
