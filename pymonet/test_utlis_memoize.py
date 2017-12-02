a8f0a2c28a0c7c17a37f125de8901ab9c2c65b10
przemyslaw <przemyslaw.jan.pietrzak@gmail.com>
2017-12-02 19:29:31 +0100

    integrate test and inplementation

--- pymonet/test_utlis_memoize.py
+++ pymonet/test_utlis_memoize.py
@@ -9,7 +9,7 @@ class MemoizeSpy:
     def fn(*args):
         return args
 
-    def key(value, new_value):
+    def key(_, value, new_value):
         return value['compare_key'] == new_value['compare_key']
 
 
@@ -41,15 +41,14 @@ def test_utils_memoize_should_call_fn_when_arguments_change(mocker):
     assert memoize_spy.fn.call_count == 2
 
 
-# def test_utils_memoize_should_cache_output_when_key_returns_truthy(mocker):
-#     memoize_spy = MemoizeSpy()
-#     mocker.spy(memoize_spy, 'fn')
-#     mocker.spy(memoize_spy, 'key')
+def test_utils_memoize_should_cache_output_when_key_returns_truthy(mocker):
+    memoize_spy = MemoizeSpy()
+    mocker.spy(memoize_spy, 'fn')
+    mocker.spy(memoize_spy, 'key')
 
-#     momoized_function = memoize(memoize_spy.fn, memoize_spy.key)
-#     result1 = momoized_function({'compare_key': 42, 'other_key': 0})
-#     result2 = momoized_function({'compare_key': 42, 'other_key': 0})
+    momoized_function = memoize(memoize_spy.fn, memoize_spy.key)
+    result1 = momoized_function({'compare_key': 42, 'other_key': 0})
+    result2 = momoized_function({'compare_key': 42, 'other_key': 1})
 
-#     assert result1 is not result2
-#     assert memoize_spy.fn.call_count == 2
-#     assert memoize_spy.key.call_count == 1
+    assert result1 is not result2
+    assert memoize_spy.fn.call_count == 1
