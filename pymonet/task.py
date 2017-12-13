869c4b3972c2d9f5f20900caa0f5c9a786fd7215
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2017-12-13 13:41:04 +0100

    update task docs

--- pymonet/task.py
+++ pymonet/task.py
@@ -7,35 +7,40 @@ class Task:
     def __init__(self, fork):
         """
         :param fork: function to call during fork
-        :type fork: Function(reject, resolve) -> any
+        :type fork: Function(reject, resolve) -> Any
         """
         self.fork = fork
 
     @classmethod
     def of(cls, value):
         """
-        :param value:
-        :type value: Any
-        :returns: instant resolved Task
-        :rtype: Task[_, resolve]
+        :param value: value to store in Task
+        :type value: A
+        :returns: resolved Task
+        :rtype: Task[Function(_, resolve) -> A]
         """
-        return lambda _, resolve: resolve(value)
+        return Task(lambda _, resolve: resolve(value))
 
     @classmethod
     def reject(cls, value):
         """
-        :param value:
+        :param value: value to store in Task
         :type value: Any
         instant rejected Task
-        :returns: Task[reject, _]
+        :returns: rejected Task 
+        :rtype: Task[Function(reject, _) -> A]
         """
-        return lambda reject, _: reject(value)
+        return Task(lambda reject, _: reject(value))
 
     def map(self, fn):
         """
+        Take function, store it and call with Task value during calling fork function.
+        Return new Task with result of called.
+
         :param fn: mapper function
-        :type fn: Function(value) -> mapped_value
-        :returns: Task[reject -> mapped_value]
+        :type fn: Function(value) -> B
+        :returns: new Task with mapped resolve attribute
+        :rtype: Task[Function(resolve, reject -> A | B]
         """
         def result(reject, resolve):
             return self.fork(
@@ -47,11 +52,13 @@ class Task:
 
     def fold(self, fn):
         """
-        Also know as flatmap.
+        Take function, store it and call with Task value during calling fork function.
+        Return result of called.
 
         :param fn: mapper function
         :type fn: Function(value) -> Task[reject, mapped_value]
-        :returns: Task[reject, mapped_value]
+        :returns:  new Task with mapper resolve attribute
+        :rtype: Task[reject, mapped_value]
         """
         def result(reject, resolve):
             return self.fork(
