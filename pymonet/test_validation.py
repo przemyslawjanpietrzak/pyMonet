54d56b558e460200c566f424cf2780d31c69dd92
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2017-12-23 20:09:16 +0100

    add validationimplementaion #1

--- pymonet/test_validation.py
+++ pymonet/test_validation.py
@@ -3,7 +3,7 @@ from pymonet.utils import increase
 from pymonet.monad_law_tester import get_associativity_test, get_left_unit_test, get_right_unit_data
 
 from hypothesis import given
-from hypothesis.strategies import texts
+from hypothesis.strategies import text
 
 import re
 
@@ -68,7 +68,7 @@ def test_validation_applicative():
         'value not contains special character'
     ])
 
-    fassert validate('s') == Validation.fail([
+    assert validate('s') == Validation.fail([
         'value not long enough'
         'value not uppercase',
         'value not contains special character'
@@ -93,25 +93,25 @@ def test_Validation_right_unit_data_law():
     get_right_unit_data(Validation.fail, [42])
 
 
-@given(texts())
+@given(text())
 def test_transform_to_box_should_return_box(integer):
     assert Validation.success(integer).to_box() == Box(integer)
     assert Validation.fail(['fail']).to_box() == Box(['fail'])
 
 
-@given(texts())
+@given(text())
 def test_transform_to_either_should_return_either(integer):
     assert Validation.success(integer).to_either() == Right(integer)
-    assert Validation.fail(['fail').to_either() == Left(['fail'])
+    assert Validation.fail(['fail']).to_either() == Left(['fail'])
 
 
-@given(texts())
+@given(text())
 def test_transform_to_lazy_should_return_lazy(integer):
     assert Validation.success(integer).to_lazy().fold(identity) == integer
-    assert Validation.fail['fail').to_lazy().fold(identity) is ['fail']
+    assert Validation.fail(['fail']).to_lazy().fold(identity) is ['fail']
 
 
-@given(texts())
+@given(text())
 def test_transform_to_try_should_return_try(integer):
     assert Validation.success(integer).to_try() == Try(integer, is_success=True)
     assert Validation.fail(['fail']).to_try() == Try(['fail'], is_success=False)
--- pymonet/validation.py
+++ pymonet/validation.py
@@ -1,6 +1,62 @@
 class Validation:
     
-    
+    def __init__(self, value, success):
+        self.value = value
+        self.success = success
+
+
     @classmethod
-    def success(value):
-        pass
+    def success(cls, value):
+        return Validation(value, True)
+
+    @classmethod
+    def fail(cls, value):
+        return Validation(value, False)
+
+    def is_success(self):
+        return self.success
+
+    def is_fail(self):
+        return not self.success
+
+    def map(self, mapper):
+        if self.success:
+            return Validation.success(mapper(self.value))
+        return Validation.fail(self.value)
+
+    def fold(self, folder):
+        if self.success:
+            return folder(self.value)
+        return Validation.fail(self.value)
+
+    def ap(self, fn):
+        fn_result = fn(self.value)
+        if fn_result.is_success():
+            if self.success:
+                return Validation.success(self.value)
+            return Validation.fail(fn_result)
+        return Validation.fail(self.value + fn_result)
+
+    def to_either(self):
+        from pymonet.either import Left, Right
+
+        if self.success:
+            return Right(self.value)
+        return Left(self.value)
+
+    def to_box(self):
+        from pymonet.box import Box
+
+        return Box(self.value)
+
+    def to_lazy(self):
+        from pymonet.lazy import Lazy
+
+        return Lazy(lambda: self.value)
+
+    def to_try(self):
+        from pymonet.monad_try import Try
+
+        if self.success:
+            return Try(self.value, is_success=True)
+        return Try(self.value, is_success=False)
