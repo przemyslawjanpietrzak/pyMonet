278b5d66bdda22a1a5d81554a682b82ba8084baf
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2017-12-24 17:22:55 +0100

    refactor monad law tester

--- pymonet/monad_law_tester.py
+++ pymonet/monad_law_tester.py
@@ -1,7 +1,3 @@
-def _get_monadic_value(monad, monad_value, use_constructor=True):
-    return monad(monad_value)
-
-
 def get_associativity_test(monadic_value, mapper1, mapper2):
     def result():
         value1 = (monadic_value
@@ -17,15 +13,15 @@ def get_associativity_test(monadic_value, mapper1, mapper2):
 
 def get_left_unit_test(monad, monad_value, mapper):
     def result():
-        monadic_value = _get_monadic_value(monad, monad_value)
+        monadic_value = monad(monad_value)
         assert monadic_value.bind(mapper) == mapper(monad_value)
 
     return result
 
 
-def get_right_unit_data(monad, monad_value, use_constructor=True):
+def get_right_unit_data(monad, monad_value):
     def result():
-        monadic_value = _get_monadic_value(monad, monad_value, use_constructor)
-        monadic_value.bind(lambda value: _get_monadic_value(monad, value, use_constructor)) == monadic_value
+        monadic_value = monad(monad_value)
+        monadic_value.bind(lambda value: monad(monad_value)) == monadic_value
 
     return result
