1801cce0bc145ae4c4aa50848ce58fe822285f34
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2017-12-24 15:06:45 +0100

    add docs string to validation monad

--- pymonet/validation.py
+++ pymonet/validation.py
@@ -5,6 +5,9 @@ class Validation:
         self.errors = errors
 
     def __eq__(self, other):
+        """
+        Two Validations are equals when values and errors lists are equal.
+        """
         return (isinstance(other, Validation) and
                 self.errors == other.errors and
                 self.value == other.value)
@@ -16,33 +19,88 @@ class Validation:
 
     @classmethod
     def success(cls, value=None):
+        """
+        Returns successful Validation with value and empty errors list.
+
+        :params value: value to store in Validation
+        :type value: A
+        :returns: Successful Validation
+        :rtype: Validation[A, []]
+        """
         return Validation(value, [])
 
     @classmethod
     def fail(cls, errors=[]):
+        """
+        Returns failed Validation with None as value and errors list.
+
+        :params errors: list of errors to store
+        :type value: List[E]
+        :returns: Failed Validation
+        :rtype: Validation[None, List[E]]
+        """
         return Validation(None, errors)
 
     def is_success(self):
+        """
+        Returns True when errors list are empty.
+
+        :returns: True for empty errors list
+        :rtype: Boolean
+        """
         return len(self.errors) == 0
 
     def is_fail(self):
+        """
+        Returns True when errors list are not empty.
+
+        :returns: True for empty errors not list
+        :rtype: Boolean
+        """
         return len(self.errors) != 0
 
     def map(self, mapper):
-        if self.success:
-            return Validation.success(mapper(self.value))
-        return Validation.fail(self.value)
+        """
+        Take function (A) -> B and applied this function on current Validation value.
+
+        :param mapper: mapper function
+        :type mapper: Function(A) -> B
+        :returns: new Validation with mapped value and previous errors
+        :rtype: Validation[B, List[E]]
+        """
+        return Validation(mapper(self.value), self.errors)
 
     def bind(self, folder):
-        if self.success:
-            return folder(self.value)
-        return Validation.fail(self.value)
+        """
+        Take function and applied this function on current Validation value and returns folder result.
+
+        :param mapper: mapper function
+        :type mapper: Function(A) -> Validation[B, E]
+        :returns: new Validation with mapped value
+        :rtype: Validation[B, E]
+        """
+        return folder(self.value)
 
     def ap(self, fn):
-        fn_result = fn(self.value)
-        return Validation(self.value, self.errors + fn_result.errors)
+        """
+        It takes as a parameter function returning another Validation.
+        Function is called with Validation value and returns new Validation with previous value
+        and concated new and old errors.
+
+        :param monad: monad contains function
+        :type monad: Function(A) -> Validation[Any, List[E]]
+        :returns: new validation with stored errors
+        :rtype: Validation[A, List[E]]
+        """
+        return Validation(self.value, self.errors + fn(self.value).errors)
 
     def to_either(self):
+        """
+        Transform Validation to Either.
+
+        :returns: Right monad with previous value when Validation has no errors, in other case Left with errors list
+        :rtype: Right[A] | Left[E]
+        """
         from pymonet.either import Left, Right
 
         if self.is_success():
@@ -50,6 +108,12 @@ class Validation:
         return Left(self.errors)
 
     def to_maybe(self):
+        """
+        Transform Validation to Maybe.
+
+        :returns: Maybe with Validation Value when Validation has no errors, in other case empty Maybe
+        :rtype: Maybe[A | None]
+        """
         from pymonet.maybe import Maybe
 
         if self.is_success():
@@ -57,18 +121,34 @@ class Validation:
         return Maybe.nothing()
 
     def to_box(self):
+        """
+        Transform Validation to Box.
+
+        :returns: Box with Validation value
+        :rtype: Box[A]
+        """
         from pymonet.box import Box
 
         return Box(self.value)
 
     def to_lazy(self):
+        """
+        Transform Validation to Try.
+
+        :returns: Lazy monad with function returning Validation value
+        :rtype: Lazy[Function() -> (A | None)]
+        """
         from pymonet.lazy import Lazy
 
         return Lazy(lambda: self.value)
 
     def to_try(self):
+        """
+        Transform Validation to Try.
+
+        :returns: successfully Try with Validation value value. Try is successful when Validation has no errors
+        :rtype: Try[A]
+        """
         from pymonet.monad_try import Try
 
-        if self.is_success():
-            return Try(self.value, is_success=True)
-        return Try(self.value, is_success=False)
+        return Try(self.value, is_success=self.is_success())
