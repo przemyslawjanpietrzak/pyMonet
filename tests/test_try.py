573dd18bcfaf92c8fe7889ad53494f3e4783fb73
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2018-01-10 17:12:34 +0100

    refactor try tests

--- tests/test_try.py
+++ tests/test_try.py
@@ -1,82 +1,102 @@
 from pymonet.monad_try import Try
+from pymonet.utils import increase
+
+from hypothesis import given
+from hypothesis.strategies import integers
+
+import pytest
 
 
 def divide(dividend, divisor):
     return dividend / divisor
 
 
-def test_try_should_call_success_callback_with_result_of_function_when_exception_was_not_thrown():
+class TrySpy:
 
-    def success_callback(value):
-        assert value == 21
+    def fn(self):
+        return 42
 
-    def fail_callback(_):
-        assert True is False
+    def fail(self):
+        return 42 / 0
 
-    (Try.of(divide, 42, 2)
-        .on_success(success_callback)
-        .on_fail(fail_callback))
+    def binder(self, value):
+        return Try.of(divide, value, 2)
 
+    def success_callback(self, value):
+        pass
 
-def test_try_should_call_fail_callback_with_result_of_function_when_exception_was_thrown():
-
-    def success_callback(_):
-        assert True is False
-
-    def fail_callback(error):
-        assert isinstance(error, ZeroDivisionError)
-        assert str(error) == 'division by zero'
-
-    (Try.of(divide, 42, 0)
-        .on_success(success_callback)
-        .on_fail(fail_callback))
+    def fail_callback(self, value):
+        pass
 
 
-def test_try_eq_should_compare_value_and_result_of_try():
-    assert Try.of(divide, 42, 2) == Try.of(divide, 42, 2)
-    assert Try.of(divide, 43, 2) != Try.of(divide, 42, 2)
-    assert Try.of(divide, 43, 1) != Try.of(divide, 42, 0)
+@pytest.fixture()
+def try_spy(mocker):
+    spy = TrySpy()
+    mocker.spy(spy, 'fn')
+    mocker.spy(spy, 'success_callback')
+    mocker.spy(spy, 'fail')
+    mocker.spy(spy, 'fail_callback')
+    mocker.spy(spy, 'binder')
+
+    return spy
 
 
-def test_try_should_appied_map_when_exception_was_thrown():
+def test_try_should_call_success_callback_with_result_of_function_when_exception_was_not_thrown(try_spy):
 
-    def success_callback(value):
-        assert value == 22
+    (Try.of(try_spy.fn)
+        .on_success(try_spy.success_callback)
+        .on_fail(try_spy.fail_callback))
 
-    def fail_callback(_):
-        assert True is False
+    assert try_spy.fail_callback.call_count == 0
+    assert try_spy.success_callback.call_count == 1
 
-    (Try.of(divide, 42, 2)
-        .map(lambda value: value + 1)
-        .on_success(success_callback)
-        .on_fail(fail_callback))
+    try_spy.success_callback.assert_called_once_with(42)
 
+def test_try_should_call_fail_callback_with_result_of_function_when_exception_was_thrown(try_spy):
 
-def test_try_should_appied_bind_when_exception_not_was_thrown():
+    (Try.of(try_spy.fail)
+        .on_success(try_spy.success_callback)
+        .on_fail(try_spy.fail_callback))
 
-    def fail_callback(_):
-        assert True is False
+    assert try_spy.fail_callback.call_count == 1
+    assert try_spy.success_callback.call_count == 0
 
-    (Try.of(divide, 42, 2)
-        .bind(lambda value: Try.of(divide, 42, 3))
-        .on_success(lambda value: value == 7)
-        .on_fail(fail_callback))
+def test_try_eq_should_compare_value_and_result_of_try(try_spy):
+    assert Try.of(try_spy.fn) == Try.of(try_spy.fn)
+    assert Try.of(try_spy.fail) != Try.of(try_spy.fail)
 
+def test_try_should_appied_map_when_exception_was_thrown(try_spy):
+    (Try.of(try_spy.fn)
+        .map(increase)
+        .on_success(try_spy.success_callback)
+        .on_fail(try_spy.fail_callback))
 
-def test_try_should_not_appied_bind_when_exception_was_thrown():
+    assert try_spy.fail_callback.call_count == 0
+    assert try_spy.success_callback.call_count == 1
 
-    def success_callback(_):
-        assert True is False
+    try_spy.success_callback.assert_called_once_with(43)
 
-    def fail_callback(error):
-        assert isinstance(error, ZeroDivisionError)
-        assert str(error) == 'division by zero'
+def test_try_should_appied_bind_when_exception_not_was_thrown(try_spy):
+    (Try.of(try_spy.fn)
+        .bind(lambda value: Try.of(divide, value, 2))
+        .on_success(try_spy.success_callback)
+        .on_fail(try_spy.fail_callback))
 
-    (Try.of(divide, 42, 0)
-        .bind(lambda value: Try.of(divide, 42, 3))
-        .on_success(success_callback)
-        .on_fail(fail_callback))
+    assert try_spy.fail_callback.call_count == 0
+    assert try_spy.success_callback.call_count == 1
 
+    try_spy.success_callback.assert_called_once_with(21)
+
+def test_try_should_not_appied_bind_when_exception_was_thrown(try_spy):
+    (Try.of(try_spy.fail)
+        .bind(lambda value: Try.of(divide, value, 2))
+        .on_success(try_spy.success_callback)
+        .on_fail(try_spy.fail_callback))
+
+    assert try_spy.fail_callback.call_count == 1
+    assert try_spy.success_callback.call_count == 0
+
+    try_spy.fail_callback.assert_called_once_with(ZeroDivisionError('division by zero',))
 
 def test_when_bind_is_rejected_monad_also_should_be_rejected():
 
