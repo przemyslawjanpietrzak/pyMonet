8548728a51e9c2c2ef179625fe015b788e767ca3
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2018-02-05 09:31:00 +0100

    apply semigroup law tester

--- testers/semigroup_law_tester.py
+++ testers/semigroup_law_tester.py
@@ -1,10 +1,11 @@
 class SemigroupLawTester:
 
-    def __init__(self, semigroup, value1, value2, value3):
+    def __init__(self, semigroup, value1, value2, value3, result):
         self.semigroup = semigroup
         self.value1 = value1
         self.value2 = value2
         self.value3 = value3
+        self.result = result
 
     def associativity_test(self):
 
@@ -13,10 +14,10 @@ class SemigroupLawTester:
                 .concat(self.semigroup(self.value3))
 
         y = self.semigroup(self.value1).concat(
-                self.semigroup(self.value2).self.semigroup(self.value3)
+                self.semigroup(self.value2).concat(self.semigroup(self.value3))
             )
 
-        assert x == y
+        assert x == y == self.result
 
     def test(self):
         self.associativity_test()
--- tests/test_semigroups.py
+++ tests/test_semigroups.py
@@ -1,6 +1,8 @@
 from hypothesis import given
 from hypothesis.strategies import text, integers, booleans
 
+from testers.semigroup_law_tester import SemigroupLawTester
+
 from pymonet.semigroups import Sum, All, First, Map
 from pymonet.utils import identity
 
@@ -11,34 +13,38 @@ ingredient3 = Map({'score': Sum(3), 'won': All(False), 'captain': First('Batman'
 
 @given(integers(), integers(), integers())
 def test_sum(x, y, z):
-    assert Sum(x).concat(Sum(y)) == Sum(x + y)
-    assert Sum(x).concat(
-        Sum(y).concat(Sum(z))
-    ) == Sum(x + y + z)
-    assert Sum(x).concat(Sum(y)).concat(Sum(z)) == Sum(x + y + z)
+    SemigroupLawTester(
+        semigroup=Sum,
+        value1=x,
+        value2=y,
+        value3=z,
+        result=Sum(x + y + z)
+    ).test()
 
 
 @given(booleans(), booleans(), booleans())
 def test_all(bool1, bool2, bool3):
-
-    assert All(bool1).concat(All(bool2)) == All(bool1 and bool2)
-    assert All(bool1).concat(
-        All(bool2).concat(All(bool3))
-    ) == All(bool1 and bool2 and bool3)
-    assert All(bool1).concat(All(bool2)).concat(All(bool3)) == All(bool1 and bool2 and bool3)
+    SemigroupLawTester(
+        semigroup=All,
+        value1=bool1,
+        value2=bool2,
+        value3=bool3,
+        result=All(bool1 and bool2 and bool3)
+    ).test()
 
 
 @given(text(), text(), text())
 def test_first(text1, text2, text3):
-    assert First(text1).concat(First(text2)) == First(text1)
-    assert First(text1).concat(First(text2)).concat(First(text3)) == First(text1)
-    assert First(text1).concat(
-        First(text2).concat(First(text3))
-    ) == First(text1)
+    SemigroupLawTester(
+        semigroup=First,
+        value1=text1,
+        value2=text2,
+        value3=text3,
+        result=First(text1)
+    ).test()
 
 
 def test_map():
-
     assert ingredient1.concat(ingredient2) == Map(
         {'score': Sum(3), 'won': All(True), 'captain': First('captain america')}
     )
