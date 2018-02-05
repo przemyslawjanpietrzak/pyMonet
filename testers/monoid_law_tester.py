1a5c89b6ea0808327dc8e85f95c3cf1360182f10
Przemyslaw Jan Pietrzak <przemyslaw.pietrzak@f-secure.com>
2018-02-05 12:02:18 +0100

    add monoid law tester

---
+++ testers/monoid_law_tester.py
@@ -0,0 +1,18 @@
+class MonoidLawTester:
+
+    def __init__(self, monoid, value):
+        self.monoid = monoid
+        self.value = value
+
+    def left_identity_test(self):
+        monoid = self.monoid(self.value)
+        assert monoid.concat(self.monoid.neutral) == monoid
+
+    def right_identity_test(self):
+        monoid = self.monoid(self.value)
+        assert self.monoid.neutral.concat(monoid) == monoid
+
+    def test(self):
+        self.left_identity_test()
+        self.right_identity_test()
+
