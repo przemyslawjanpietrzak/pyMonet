ff613acf52ed2437a00fe1d6e07fe09460d5874b
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2018-05-06 09:57:07 +0200

    immutable_list : find

--- pymonet/immutable_list.py
+++ pymonet/immutable_list.py
@@ -1,17 +1,15 @@
-class EmptyImmutableList:
-    pass
-
-
 class ImmutableList:
 
-    def __init__(self, head, tail=None):
+    def __init__(self, head=None, tail=None, is_empty=False):
         self.head = head
         self.tail = tail
+        self.is_empty = is_empty
 
     def __eq__(self, other):
         return isinstance(other, ImmutableList) \
             and self.head == other.head\
-            and self.tail == self.tail
+            and self.tail == other.tail\
+            and self.is_empty == other.is_empty
 
     def __str__(self):
         return 'ImmutableList{}'.format(self.to_list())
@@ -25,10 +23,15 @@ class ImmutableList:
             ImmutableList.of(elements[0], *elements[1:])
         )
 
+    @classmethod
+    def empty(cls):
+        return ImmutableList(is_empty=True)
+
     @property
     def length(self):
         if self.tail is None:
             return 1
+
         return self.tail.length + 1
 
     def to_list(self):
@@ -41,6 +44,7 @@ class ImmutableList:
         def acc(elemet, head, tail):
             if tail is None:
                 return ImmutableList(head, ImmutableList(elemet))
+
             return ImmutableList(elemet, acc(head, tail))
 
         return acc(new_element, self.head, self.tail)
@@ -62,9 +66,21 @@ class ImmutableList:
 
     def filter(self, fn):
         if self.tail is None:
-            return ImmutableList(self.head if fn(self.head) else None)  # TODO empty
+            if fn(self.head):
+                return ImmutableList(self.head)
+            return ImmutableList(is_empty=True)
 
         if fn(self.head):
             return ImmutableList(self.head, self.tail.filter(fn))
 
         return self.tail.filter(fn)
+
+    def find(self, fn):
+        if self.tail is None:
+            return self.head if fn(self.head) else None
+        
+        if fn(self.head):
+            return self.head
+
+        return self.tail.find(fn)
+
