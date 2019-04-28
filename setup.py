d91eea8b7d7530303728d8e1ab7820932744b0d7
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2019-04-28 15:46:26 +0200

    release 0.11.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 setup(
   name='pymonet',
   packages=['pymonet'],
-  version='0.10.0',
+  version='0.11.0',
   description='High abstract python library for functional programming. Contains algebraic data structures known from Haskell or Scala.',
   author='Przemyslaw Jan Pietrzak',
   author_email='przemyslaw.jan.pietrzak@gmail.com',
   url='https://github.com/przemyslawjanpietrzak/pyMonet',
-  download_url='https://github.com/przemyslawjanpietrzak/pyMonet/archive/0.10.0.tar.gz',
+  download_url='https://github.com/przemyslawjanpietrzak/pyMonet/archive/0.11.0.tar.gz',
   keywords=['functional-programming', 'monads', 'functors', 'semigroups'],
   classifiers=[],
 )
