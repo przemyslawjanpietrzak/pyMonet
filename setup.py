f132cfa91d37bef56203baa58049f437a86ea3c2
user <przemyslaw.jan.beiger@gmail.com>
2020-04-19 20:21:07 +0200

    release 0.12.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name='pymonet',
   packages=['pymonet'],
-  version='0.11.2',
+  version='0.12.0',
   description='High abstract python library for functional programming. Contains algebraic data structures known from Haskell or Scala.',
   author='Przemyslaw Jan Pietrzak',
   author_email='przemyslaw.jan.pietrzak@gmail.com',
